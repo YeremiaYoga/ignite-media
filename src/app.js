9459053c1b1a5b272885f82e5f8a7c67d0d1707a
Yeremia Yoga <yogayeremia@gmail.com>
2025-10-23 12:01:41 +0700

    first commit

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import cookieParser from "cookie-parser";
 import path from "path";
 import dotenv from "dotenv";
 import uploadRoutes from "./routes/uploadRoutes.js";
-
+import assetRoutes from "./routes/assetsRoutes.js";
 dotenv.config();
 
 const app = express();
@@ -24,6 +24,7 @@ app.use(
 );
 
 app.use(express.static(path.resolve("public")));
+app.use("/api/assets", assetRoutes);
 app.use("/upload", uploadRoutes);
 
 export default app;
